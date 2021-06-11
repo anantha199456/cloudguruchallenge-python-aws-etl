8f81cd4780f73b7884484ef19f2247457e7b479c
Krishna's Mac <anantha@Krishnass-MacBook-Air.local>
2021-06-11 14:49:56 +0530

    GitHub Action terraform.yml file added

--- setups/mail.tf
+++ setups/mail.tf
@@ -1,12 +1,20 @@
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  #access_key = ""
-  #secret_key = ""
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 terraform {
   required_version = ">= 0.13.4"
+
+  backend "remote" {
+    organization = "acg-etl"
+
+    workspaces {
+      name = "cgc-etl"
+    }
+  }
 }
 
 module "sns" {
